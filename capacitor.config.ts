a9adaf1756eec934ed79b2e8bed50f69736f3c5e
Philippe N. <philippesteve2.ps@gmail.com>
2024-10-05 14:11:52 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ec2lt_gps',
+  webDir: 'www'
+};
+
+export default config;
